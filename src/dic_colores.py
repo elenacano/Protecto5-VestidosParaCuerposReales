a6c72caf2cf2d5030c82abf425b828a09e459d0a
elenacano <elena.canocas@gmail.com>
2024-11-01 11:43:26 +0100

    apis terminadas

---
+++ src/dic_colores.py
@@ -0,0 +1,52 @@
+colores = {
+    'SILVER': 'plata',
+    'BLACK': 'negro',
+    'PINK': 'rosa',
+    'MID BLUE': 'azul',
+    'LIGHT BLUE': 'azul',
+    'CREAM': 'crema',
+    'YELLOW': 'amarillo',
+    'WHITE': 'blanco',
+    'ORANGE': 'naranja',
+    'PURPLE': 'morado',
+    'BROWN': 'marrón',
+    'BEIGE': 'beige',
+    'GREY': 'gris',
+    'BLUE': 'azul',
+    'NAVY': 'azul',
+    'GREEN': 'verde',
+    'RED': 'rojo',
+    'DARK BROWN': 'marrón',
+    'GREEN/RED': 'verde/rojo',
+    'GREEN/MULTI': 'verde',
+    'IVORY': 'beige',
+    'TAUPE': 'topo',
+    'DARK RED': 'rojo',
+    'RED/MULTI': 'rojo',
+    'MERLOT': 'granate',
+    'MAROON': 'granate',
+    'SANDSHELL': 'beige',
+    'OLIVE': 'verde',
+    'EMERALD': 'verde',
+    'EGRET': 'blanco',
+    'PINK/MULTI': 'rosa',
+    'BLACK/BLACK': 'negro',
+    'HEATHER GREY': 'gris',
+    'BURGUNDY': 'granate',
+    'HUNTER GREEN': 'verde',
+    'WINE': 'granate',
+    'BLACK/NUDE': 'negro',
+    'OLIVINE /BLACK': 'verde',
+    'BLACK/MULTI': 'negro',
+    'RED/WHITE': 'rojo/blanco',
+    'VANILLA': 'beige'
+}
+
+colores_traduccion = {
+    
+    
+
+    'BLACK/MULTI': 'negro/multicolor',
+    'RED/WHITE': 'rojo/blanco',
+    'VANILLA': 'vainilla'
+}
--- src/funciones_auxiliares.py
+++ src/funciones_auxiliares.py
@@ -1,26 +1,26 @@
 import json
 import os
 
-colores = {
-    'SILVER': 'plata',
-    'BLACK': 'negro',
-    'PINK': 'rosa',
-    'MID BLUE': 'azul medio',
-    'LIGHT BLUE': 'azul claro',
-    'CREAM': 'crema',
-    'YELLOW': 'amarillo',
-    'WHITE': 'blanco',
-    'ORANGE': 'naranja',
-    'PURPLE': 'morado',
-    'BROWN': 'marrón',
-    'BEIGE': 'beige',
-    'GREY': 'gris',
-    'BLUE': 'azul',
-    'NAVY': 'azul marino',
-    'GREEN': 'verde',
-    'RED': 'rojo',
-    'DARK BROWN': 'marrón oscuro'
-}
+# colores = {
+#     'SILVER': 'plata',
+#     'BLACK': 'negro',
+#     'PINK': 'rosa',
+#     'MID BLUE': 'azul medio',
+#     'LIGHT BLUE': 'azul claro',
+#     'CREAM': 'crema',
+#     'YELLOW': 'amarillo',
+#     'WHITE': 'blanco',
+#     'ORANGE': 'naranja',
+#     'PURPLE': 'morado',
+#     'BROWN': 'marrón',
+#     'BEIGE': 'beige',
+#     'GREY': 'gris',
+#     'BLUE': 'azul',
+#     'NAVY': 'azul marino',
+#     'GREEN': 'verde',
+#     'RED': 'rojo',
+#     'DARK BROWN': 'marrón oscuro'
+# }
 
 def creacion_diccionario_asos(dic_precio):
     dic_vestido = {
@@ -54,5 +54,39 @@ def creacion_diccionario_asos(dic_precio):
             dic_vestido["color"].append(color)
             dic_vestido["talla"].append(talla)
             dic_vestido["disponible"].append(dispomible)
+
+    return dic_vestido
+
+def creacion_diccionario_forever21():
+    dic_vestido = {
+        "nombre" : [],
+        "marca" : [],
+        "precio" : [],
+        "color" : [],
+        "talla" : [],
+        "stock" : []
+    }
+
+    for archivo in os.listdir("../datos/api_forever21")[:-1]:
+
+        with open(f"../datos/api_forever21/{archivo}", 'r') as file:
+            producto = json.load(file)
+
+        id = int(archivo.split("_")[0])
+        marca = "Forever21"
+        for i in range(len(producto["product"]["Variants"])):
+            for j in range(len(producto["product"]["Variants"][i]["Sizes"])):
+                nombre = producto["product"]["Variants"][i]["Sizes"][j]["DisplayName"]
+                color = producto["product"]["Variants"][i]["ColorName"]
+                talla = producto["product"]["Variants"][i]["Sizes"][j]["SizeName"]
+                precio = producto["product"]["Variants"][i]["Sizes"][j]["Price"]
+                stock = producto["product"]["Variants"][i]["Sizes"][j]["inventories"][0]["stock_level"]
+
+                dic_vestido["nombre"].append(nombre)
+                dic_vestido["marca"].append(marca)
+                dic_vestido["precio"].append(precio)
+                dic_vestido["color"].append(color)
+                dic_vestido["talla"].append(talla)
+                dic_vestido["stock"].append(stock)
 
     return dic_vestido
